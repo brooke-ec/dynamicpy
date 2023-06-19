1abf99bbd9d28cd8c39914a247b1ea22f1c48287
NimajnebEC <nimajnebec@users.noreply.github.com>
2023-06-19 15:26:20 +0100

    Add type_handler functions for utility

--- src/dynamicpy/loader.py
+++ src/dynamicpy/loader.py
@@ -1,6 +1,8 @@
 import logging
 from types import ModuleType
-from typing import Callable, Optional, Union
+from typing import Any, Callable, Optional, Type, TypeVar, Union
+
+from typeguard import TypeCheckError, check_type
 
 from dynamicpy import utils
 
@@ -8,19 +10,19 @@ __all__ = ("DynamicLoader",)
 
 _log = logging.getLogger(__name__)
 
-Handler = Callable[[str, object], None]
-
+T = TypeVar("T")
+Handler = Callable[[str, T], None]
 Selector = Callable[[str, object], bool]
 
 
 class DynamicLoader:
-    """An instance of `DynamicLoader` can be used to dynamically import modules and handle their globals through registered handlers."""
+    """An instance of `DynamicLoader` can be used to dynamically import modules and handle their attributes through registered handlers."""
 
     def __init__(self) -> None:
-        """An instance of `DynamicLoader` can be used to dynamically import modules and handle their globals through registered handlers."""
+        """An instance of `DynamicLoader` can be used to dynamically import modules and handle their attributes through registered handlers."""
         self._handlers: list[_SelectorHandlerPair] = []
 
-    def register_handler(self, handler: Handler, selector: Optional[Selector] = None):
+    def register_handler(self, handler: Handler[Any], selector: Optional[Selector] = None):
         """Register a handler for the specified selector.
 
         Parameters
@@ -28,22 +30,59 @@ class DynamicLoader:
         handler : Callable[[str, object], None]
             The handler to be called if the predicate returns `True`.
         selector : Callable[[str, object], bool], optional
-            A predicate that will be run against every global. If `True` is returned then the global passed to the handler.
+            A predicate that will be run against every attribute. If `True` is returned then the atribute passed to the handler.
         """
         selector = selector or (lambda x, y: True)
         self._handlers.append(_SelectorHandlerPair(selector, handler))
 
-    def handler(self, selector: Optional[Selector] = None) -> Callable[[Handler], None]:
+    def handler(
+        self, selector: Optional[Selector] = None
+    ) -> Callable[[Handler[Any]], None]:
         """A wrapper around around the `register_handler` function to be used as a decorator.
 
         Parameters
         ----------
         selector : Selector
-            A predicate that will be run against every global. If `True` is returned then the global passed to the handler.
+            A predicate that will be run against every attribute. If `True` is returned then the atribute passed to the handler.
         """
 
-        def decorator(handler: Handler) -> None:
+        def decorator(handler: Handler[Any]) -> None:
             self.register_handler(handler, selector)
+
+        return decorator
+
+    def register_type_handler(self, handler: Handler[T], type: Type[T]):
+        """Register a handler that matches for the specified type.
+
+        Parameters
+        ----------
+        handler : Callable[[str, object], None]
+            The handler to be called if the attribute matches the specified type.
+        type : Type
+            The type to match against.
+        """
+
+        def selector(_, value: object):
+            try:
+                check_type(value, type)
+            except TypeCheckError:
+                return False
+            else:
+                return True
+
+        self.register_handler(handler, selector)
+
+    def type_handler(self, type: Type[T]) -> Callable[[Handler[T]], None]:
+        """A wrapper around around the `register_type_handler` function to be used as a decorator.
+
+        Parameters
+        ----------
+        type : Type
+            The type to run the handler on if an assignable attribute is found.
+        """
+
+        def decorator(handler: Handler[T]) -> None:
+            self.register_type_handler(handler, type)
 
         return decorator
 
@@ -114,6 +153,6 @@ class _SelectorHandlerPair:
         self.handler = handler
 
     def handle(self, name: str, value: object):
-        # Run handler if selector applies to global
+        # Run handler if selector applies to attribute
         if self.selector(name, value):
             self.handler(name, value)
